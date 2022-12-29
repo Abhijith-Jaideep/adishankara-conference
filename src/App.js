e44ca02a0096d03fd967cfaf7e05fd8185e7292d
Abhijith Jaideep <abhijithjaideep176@gmail.com>
2022-12-29 14:06:59 +0530

    Added about college component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AboutCollege from './components/AboutCollege/AboutCollege';
 import AboutConf from './components/AboutConf/AboutConf';
 import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero'
@@ -8,6 +9,7 @@ function App() {
    <div>
     <Hero/>
     <AboutConf />
+    <AboutCollege/>
     <Footer/>
    </div>
   );
