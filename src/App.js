99027b905b6378e7f656a2fe992cf96aa55d2147
Ajay Antu <ajaytechy7@gmail.com>
2022-12-29 15:13:06 +0530

    added important dates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import AboutConf from './components/AboutConf/AboutConf';
 import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero'
+import ImportantDates from './components/ImportantDates/ImportantDates';
 
 function App() {
   return (
    <div>
     <Hero/>
     <AboutConf />
+    <ImportantDates />
     <Footer/>
    </div>
   );
---
+++ src/components/ImportantDates/ImportantDates.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import "./ImportantDates.css"
+
+export default function ImportantDates() {
+  return (
+    <>
+        <div className="impdates_main_container">
+
+            <div className="impdates_container">
+                <h1 className="dates_title">Important Dates</h1>
+                <div className="impdates_table" style={{overflowX:"auto"}}>
+                    <table>
+                        
+                        <tr>
+                            <td>Paper submission</td>
+                            <td>20th June 2022</td>
+                            <td className='status'><span>Active</span></td>
+                        </tr>
+                        <tr>
+                            <td>Paper submission Deadline</td>
+                            <td>10th Sep 2022</td>
+                            <td className='status'><span>Active</span></td>
+                        </tr>
+                        <tr>
+                            <td>Acceptance Notification starts</td>
+                            <td>15th Nov 2022</td>
+                            <td className='status'><span>Active</span></td>
+                        </tr>
+                        <tr>
+                            <td>Early Bird Registration</td>
+                            <td>07th Dec 2022</td>
+                            <td className='status'><span>Active</span></td>
+                        </tr>
+                        <tr>
+                            <td>Registration closes</td>
+                            <td>12th Dec 2022</td>
+                            <td className='status'><span>Active</span></td>
+                        </tr>
+                        <tr>
+                            <td>Camera ready copy submission</td>
+                            <td>10th Dec 2022</td>
+                            <td className='status'><span>Active</span></td>
+                        </tr>
+                        <tr>
+                            <td>Pre conference Workshop</td>
+                            <td>04th Jan 2023</td>
+                            <td className='status'><span>Active</span></td>
+                        </tr>
+                        <tr className='conference'>
+                            <td>Conference</td>
+                            <td>04th Jan 2023</td>
+                            <td className='status_conf'><span>Not Active</span></td>
+                        </tr>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </>
+  )
+}
